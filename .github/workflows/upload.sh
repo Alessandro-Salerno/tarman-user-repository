959ccb2ec6a88dd4efb28733e584ce86feb91a34
Alessandro-Salerno <oxygen05@outlook.it>
2024-11-08 19:57:24 +0100

    arch-specific repos

--- .github/workflows/upload.sh
+++ .github/workflows/upload.sh
@@ -2,23 +2,23 @@ git config --local user.email "41898282+github-actions[bot]@users.noreply.github
 git config --local user.name "github-actions[bot]"
 git checkout --orphan latest-repos
 git rm -rf *.md
-# cp .github/workflows/latest-linux-bundle.md ./README.md
+# cp .github/workflows/latest-linux-x86_64-bundle.md ./README.md
 git rm -rf .github
 
-mv macos/ tarman-user-repository
-tar -czvf macos.tar.gz tarman-user-repository/
+mv macos-arm64/ tarman-user-repository
+tar -czvf macos-arm64.tar.gz tarman-user-repository/
 rm -rf tarman-user-repository
-git rm -rf macos/
+git rm -rf macos-arm64/
 git rm -rf tarman-user-repository/
 
-mv linux/ tarman-user-repository
-tar -czvf linux.tar.gz tarman-user-repository/
+mv linux-x86_64/ tarman-user-repository
+tar -czvf linux-x86_64.tar.gz tarman-user-repository/
 rm -rf tarman-user-repository
-git rm -rf linux/
+git rm -rf linux-x86_64/
 git rm -rf tarman-user-repository/
 
-git add macos.tar.gz
-git add linux.tar.gz
+git add macos-arm64.tar.gz
+git add linux-x86_64.tar.gz
 
 git commit -m "new bundle"
 git push --set-upstream origin latest-repos --force
